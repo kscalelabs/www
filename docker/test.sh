bc67158ed7eac69703a6749e8375ba72bcfb54b0
Benjamin Bolte <ben@bolte.cc>
2025-01-06 21:09:48 -0800

    update docker

--- docker/test.sh
+++ docker/test.sh
@@ -1,14 +1,19 @@
+#!/bin/bash
 # Script for testing the docker container.
+
+script_dir=$(dirname "$0")
+ecr_path="725596835855.dkr.ecr.us-east-1.amazonaws.com"
+image="${ecr_path}/www/staging:latest"
 
 # Gets the login password for the AWS ECR and logs in to the ECR.
 aws ecr get-login-password --profile kscale --region us-east-1 |
-    docker login --username AWS --password-stdin 725596835855.dkr.ecr.us-east-1.amazonaws.com
+    docker login --username AWS --password-stdin ${ecr_path}
 
 # Pulls the latest docker container.
-docker pull 725596835855.dkr.ecr.us-east-1.amazonaws.com/www/staging:latest
+docker pull --platform=linux/amd64 ${image}
 
 # Runs the docker container.
 docker run \
     -P \
-    --env-file local_docker.env \
-    725596835855.dkr.ecr.us-east-1.amazonaws.com/www/staging:latest
+    --env-file "${script_dir}/local_docker.env" \
+    ${image}
